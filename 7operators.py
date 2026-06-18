46512213752fae5f95ac62218cf4376a5f0169c7
earth037 <prithviprasad91@gmail.com>
2026-06-19 02:02:02 +0530

    Operators

---
+++ 7operators.py
@@ -0,0 +1,59 @@
+#Arithematic Operators
+a = 2
+b = 3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a**b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+"""
+priority
+()
+**
+* / // %
++ -
+"""
+
+#Comparison Operators
+a = 2
+b = 3
+c = 2
+print(a==b)
+print(a==c)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=c)
+print(a<=c)
+
+#Logical Operators
+a = 2
+b = 3
+print(a>b and a<b)
+print(a>b or a<b)
+print(not a>b and a<b)
+
+#Assignment Operators
+a = 2
+b = 3
+a = a + b
+print(a)
+a = 2
+b = 3
+a += b
+print(a)
+a -= b
+print(a)
+a *= b
+print(a)
+a /= b
+print(a)
+a **= b
+print(a)
+a %= b
+print(a)
+a //= b
+print(a)
