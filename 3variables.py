a3fbb1c77fb27f543c918952cd4e4c7e7570edd7
earth037 <prithviprasad91@gmail.com>
2026-06-18 23:52:20 +0530

    comments & variables

---
+++ 3variables.py
@@ -0,0 +1,24 @@
+name = "prithvi"
+age = 22
+print(name)
+print(age)
+
+# Rules
+# 1) can't start with a number
+# 1name
+# 2) no spaces allowed
+# my name = "prithvi"
+#  myname = "prithvi"
+# 3) can't use special characters except _ (underscore)
+# my-name = "prithvi"
+
+#Naming Conventions
+# 1) snake_case
+my_name = "prithvi"
+my_age = 22
+# 2) camelCase
+myName = "prithvi"
+myAge = 22
+# 3) PascalCase
+MyName = "prithvi"
+MyAge = 22
