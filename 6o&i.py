878720ee65677d9ab63ca9cdc65963d89822c53c
earth037 <prithviprasad91@gmail.com>
2026-06-19 01:34:10 +0530

    Input and output

---
+++ 6o&i.py
@@ -0,0 +1,13 @@
+#Output
+name = "prithvi"
+age = 22
+
+print("hello") # basic output
+print(name) # outputting a variable
+print(f"my name is {name} and my age is {age}") # outputting a variable using f-string (formatted string literals)
+print("my name is", name, "and my age is", age) # outputting a variable using comma separated values
+
+#Input
+name = input("Enter your name: ") # taking input from the user and storing it in a variable
+age = int(input("Enter your age: ")) # taking input from the user and storing it in a variable
+print(f"my name is {name} and my age is {age+1}") # outputting a variable using f-string (formatted string literals)
