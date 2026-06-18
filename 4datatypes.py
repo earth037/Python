0f2d90ca8c9817fd6a04e64729de7b0c02c67308
earth037 <prithviprasad91@gmail.com>
2026-06-19 00:09:49 +0530

    Data Types

---
+++ 4datatypes.py
@@ -0,0 +1,37 @@
+#Integer data type - 0 to infinity and -1 to negative infinity
+a = 22
+b = -23
+c = 3456
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Float data type - decimal numbers and in fractional form
+d = 3.14
+e = -0.001
+f = 22/7
+print(type(d))
+print(type(e))
+print(type(f))
+
+#Complex data type - used to represent complex numbers in the form of a + bj
+g = 2 + 3j
+h = -1 - 4j
+print(type(g))
+print(type(h))
+
+#String data type - used to represent textual data, can store letters, numbers, and special characters
+i = "Hello, World!"
+j = 'Python is "great" 45645 !@((#@#$'
+print(type(i))
+print(type(j))
+
+#Boolean data type - used to represent truth values, can be either True or False
+k = True
+l = False
+print(type(k))
+print(type(l))
+
+#None data type - used to represent the absence of a value or a null value
+m = None
+print(type(m))
