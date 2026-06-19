43ecd2ce8e0707213516604fdd9b4d479cef2bdd
earth037 <prithviprasad91@gmail.com>
2026-06-20 02:52:14 +0530

    tuple and set

---
+++ 13tuple.py
@@ -0,0 +1,13 @@
+# days = ("mon","tue","wed")
+# print(days)
+# print(days[-1])
+
+# t = (1,2,3,3,4,3,2)
+# print(t.index(2))
+# print(t.count(3))
+
+# def students():
+#     return "prithvi",22,"mumbai"
+# info = students()
+# name, age, city = info
+# print(city)
---
+++ 14set.py
@@ -0,0 +1,33 @@
+# l = [1,2,3,3,3,2,2,4,5,2,6,4,5,7]
+# s = set(l)
+# print(s)
+
+# d = {}
+# print(type(d))
+# s = {1}
+# print(type(s))
+
+a = {1,2,3,4}
+b = {3,4,5,6}
+c = {3,4}
+# a.add(4)
+# a.add(5)
+# print(a)
+# a.clear()
+# print(a)
+# print(a-b)
+# print(a.difference(b))
+# a -= b
+# print(a)
+# a.discard(2)
+# print(a)
+# print(a&b)
+# a &= b
+# print(a.isdisjoint(b))
+# print(a<=b)
+# print(c<=b)
+# print(b>=c)
+# p = a.pop()
+# print(p,a)
+# print(a^b)
+# print(a|b)
